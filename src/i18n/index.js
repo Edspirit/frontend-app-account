f2a0e152cfa4669863c834f01e26fed3ef994f6c
Mahan <mahan.abbuspour@gmail.com>
2023-03-04 18:43:23 +0330

    Fix: add fa language to i18n/index

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -3,7 +3,7 @@ import caMessages from './messages/ca.json';
 // no need to import en messages-- they are in the defaultMessage field
 import es419Messages from './messages/es_419.json';
 import frMessages from './messages/fr.json';
-import faIRMessages from './messages/fa.json';
+import faMessages from './messages/fa.json';
 import zhcnMessages from './messages/zh_CN.json';
 import heMessages from './messages/he.json';
 import idMessages from './messages/id.json';
@@ -17,7 +17,7 @@ import ukMessages from './messages/uk.json';
 const messages = {
   ar: arMessages,
   'es-419': es419Messages,
-  fa: faIRMessages,
+  fa: faMessages,
   fr: frMessages,
   'zh-cn': zhcnMessages,
   ca: caMessages,
